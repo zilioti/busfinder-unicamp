dc70f74decdf5e5eb5cb3d0557be30c3e01e6c55
Murilo <91murilo@gmail.com>
2012-05-10 01:34:30 -0300

    Projeto no Ar!

---
+++ BusFinderUnicamp/src/br/unicamp/busfinder/BusFinderUnicampActivity.java
@@ -0,0 +1,13 @@
+package br.unicamp.busfinder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BusFinderUnicampActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
